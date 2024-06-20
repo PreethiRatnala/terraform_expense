c1da5be8af97d925037a368d9d7a19d6d6233eba
PreethiRatnala <preethi.ratnala64@gmail.com>
2024-06-20 21:18:52 +0530

    dynammic block

--- terraform/dynamic-block/variabale.tf
+++ terraform/dynamic-block/variabale.tf
@@ -31,6 +31,21 @@ variable "inbound_rules" {
             port = 22,
             allowed_cidr = ["0.0.0.0/0"]
             protocol = "tcp"
+        },
+        {
+            port = 80,
+            allowed_cidr = ["0.0.0.0/0"]
+            protocol = "tcp"
+        },
+        {
+            port = 8080,
+            allowed_cidr = ["0.0.0.0/0"]
+            protocol = "tcp"
+        },
+        {
+            port = 3306,
+            allowed_cidr = ["0.0.0.0/0"]
+            protocol = "tcp"
         }
     ]
 }
