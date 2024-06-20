45f4a6df4403d681c600e84743d57130f7b0b235
PreethiRatnala <preethi.ratnala64@gmail.com>
2024-06-20 19:50:32 +0530

    remote-state

--- terraform/remote-state/ec2.tf
+++ terraform/remote-state/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "db1" {
-  ami = "ami-090252cbe067a9e58"
+  ami = "ami-031d574cddc5bb371"
   vpc_security_group_ids = ["sg-0f9620af225d21c6a"]
   instance_type = "t3.micro"
 
