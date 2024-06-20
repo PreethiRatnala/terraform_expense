758d16d703d32e64f91b671e0f1e2309f88222b7
PreethiRatnala <preethi.ratnala64@gmail.com>
2024-06-20 20:10:06 +0530

    remote state

--- terraform/for_each/provider.tf
+++ terraform/for_each/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "devopspractice-remote-state"
-    key    = "remote-state-demo"
+    key    = "remote-state-demo1"
     region = "us-east-1"
     dynamodb_table = "remote-data-lock"
   }
